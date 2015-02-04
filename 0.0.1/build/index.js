d61612f4d8a7f0e0d2be0780b1319473f7397364
kissyteam <tiehang.lth@taobao.com>
2015-02-04 20:47:09 +0800

    sync daily version 0.0.1

---
+++ 0.0.1/build/index.js
@@ -0,0 +1,33 @@
+/*
+Thu Oct 09 2014 10:53:41 GMT+0800 (CST)
+combined files by KMD:
+
+index.js
+*/
+
+KISSY.add('kg/dd/2.0.0/index',["node","base"],function(S ,require, exports, module) {
+var $ = require('node').all;
+var Base = require('base');
+
+var Dd = Base.extend({
+    initializer:function(){
+        var self = this;
+        var $target = self.get('$target');
+    }
+},{
+    ATTRS:{
+        $target:{
+            value:'',
+            getter:function(v){
+                return $(v);
+            }
+        }
+    }
+});
+
+module.exports = Dd;
+
+
+
+
+});
